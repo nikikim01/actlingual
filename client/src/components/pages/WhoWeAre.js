b354c8dbb9209a8a18f04f05548b486059bd1713
Niki Kim <nicolegkim@gmail.com>
2021-02-17 00:03:05 -0800

    bio pics

--- client/src/components/pages/WhoWeAre.js
+++ client/src/components/pages/WhoWeAre.js
@@ -26,69 +26,59 @@ class WhoWeAre extends Component {
             
             <div className="WhoWeAre-bioRow">                 
              <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Omar_Santiago_Reyes_President.png"></img>
+                <text style={{marginBottom: "1vh"}}>Omar Santiago Reyes</text>
+                <text style={{marginBottom: "1vh"}}>President</text>
                 </div>                
                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Tam_Nguyen_Vice_President.png"></img>
+                <text style={{marginBottom: "1vh"}}>Tam Nguyen</text>
+                <text>Vice President</text>
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Caroline_Bao_Treasurer_and_AMIC_Leader.png"></img>
+                <text style={{marginBottom: "1vh"}}>Caroline Bao</text>
+                <text>Treasurer | AMIC Leader</text>
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto", overflow: "hidden"}}src="https://storage.googleapis.com/actlingual/Nicole_Niki_Kim_Webmaster.png"></img>
+                <text style={{marginBottom: "1vh"}}>Nicole (Niki) Kim</text>
+                <text>Webmaster</text>
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Silvia_Cho_Secretary.png"></img>
+                <text style={{marginBottom: "1vh"}}>Silvia Cho</text>
+                <text>Secretary</text>
+                
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Lulu_Tian_ACC_Leader.png"></img>
+                <text style={{marginBottom: "1vh"}}>Lulu Tian</text>
+                <text>ACC Leader</text>
                 </div>
+                
             </div>
             <div className="WhoWeAre-bioRow">
             <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Bhav_Jain_CHEDAR_Leader.png"></img>
+                <text style={{marginBottom: "1vh"}}>Bhav Jain</text>
+                <text style={{marginBottom: "1vh"}}>CHEDAR Leader</text>
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Jessica_Jimenez_ACC_Leader.png"></img>
+                <text style={{marginBottom: "1vh"}}>Jessica Jimenez</text>
+                <text>ACC Leader</text>
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Victor_Damptey_AMIC_Leader.png"></img>
+                <text style={{marginBottom: "1vh"}}>Victor Damptey</text>
+                <text>AMIC Leader</text>
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Sky_Kim_Social_Chair.png"></img>
+                <text style={{marginBottom: "1vh"}}>Sky Kim</text>
+                <text>Social Chair</text>
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Tina_Chen_Social_Chair.png"></img>
+                <text style={{marginBottom: "1vh"}}>Tina Chen</text>
+                <text>Social Chair</text>
                 </div>                 <div className='WhoWeAre-bioContainer' style={{display:'flex', flexDirection:'column'}}>
-                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/comingsoonimage.jpg"></img>
-                <text>NAME HERE</text>
-                <br/>
-                <text>POSITION TYPE</text>
+                 <img style={{height:"25vh", padding: "1vh", display: "block", marginLeft:"auto", marginRight:"auto"}}src="https://storage.googleapis.com/actlingual/Mydia_Phan_AAIVote_Leader.png"></img>
+                <text style={{marginBottom: "1vh"}}>Mydia Phan</text>
+                <text >AAI Vote Leader</text>
                 </div>
             </div>
 
